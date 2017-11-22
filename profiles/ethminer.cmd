b8a6444fbe3199095a00ef68ed4c7dac7f4b225e
lcluber <ludovic.cluber@gmail.com>
2017-11-22 21:49:51 +0100

    added troubleshooting guide for mining with 6 gpus and more on ethminer

--- profiles/ethminer.cmd
+++ profiles/ethminer.cmd
@@ -3,6 +3,13 @@
 
 :: call "%GIT_INSTALL_ROOT%/cmd/start-ssh-agent.cmd"
 :: set "PATH=%CMDER_ROOT%\vendor\whatever;%PATH%"
+
+:: In case of memory problems : 
+:: - https://www.geeksinphoenix.com/blog/post/2016/05/10/how-to-manage-windows-10-virtual-memory.aspx
+:: - 24 GB for 6 GPUs
+:: - setx GPU_MAX_ALLOC_PERCENT 100
+:: - setx GPU_SINGLE_ALLOC_PERCENT 100
+
 @echo on
 set FOLDER=C:\Users\username\ethminer\bin\
 set POOL=http://noobpool.com:8888
