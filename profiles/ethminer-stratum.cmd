6c106b98ccf30c7840b95b82c9a9756efb632cfd
lcluber <ludovic.cluber@gmail.com>
2017-12-03 22:53:44 +0100

    added ethminer profile for pools with fallback servers

--- profiles/ethminer-stratum.cmd
+++ profiles/ethminer-stratum.cmd
@@ -3,19 +3,13 @@
 
 :: call "%GIT_INSTALL_ROOT%/cmd/start-ssh-agent.cmd"
 :: set "PATH=%CMDER_ROOT%\vendor\whatever;%PATH%"
-
-:: In case of memory problems : 
-:: - https://www.geeksinphoenix.com/blog/post/2016/05/10/how-to-manage-windows-10-virtual-memory.aspx
-:: - 24 GB for 6 GPUs
-:: - setx GPU_MAX_ALLOC_PERCENT 100
-:: - setx GPU_SINGLE_ALLOC_PERCENT 100
-
 @echo on
 set FOLDER=C:\Users\username\ethminer\bin\
-set POOL=http://noobpool.com:8888
+set POOL1=eu1.ethermine.org:4444
+set POOL2=us1.ethermine.org:4444
 set WALLET=0x69593a8875894bed5665a71d8bd0fdd9980cac93
 set WORKERNAME=Worker2
 set CONFIG=-U --farm-recheck 200
-set CMD=ethminer -F %POOL%/%WALLET%/%WORKERNAME% %CONFIG%
+set CMD=ethminer -S %POOL1% -FS %POOL2% -O %WALLET%.%WORKERNAME% %CONFIG%
 cd %FOLDER%
 %CMD%
