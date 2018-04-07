a77c806095ddfe935fe5fd134abcd2411cf80541
lcluber <ludovic.cluber@gmail.com>
2018-04-07 11:10:47 +0200

    fix ethminer-ubiq profile

--- profiles/ethminer-ubiq.cmd
+++ profiles/ethminer-ubiq.cmd
@@ -5,7 +5,7 @@
 :: set "PATH=%CMDER_ROOT%\vendor\whatever;%PATH%"
 @echo on
 set FOLDER=C:\Users\username\ethminer\bin\
-set POOL=stratum+tcp://eu.ubiqpool.io:8888
+set POOL=http://eu.ubiqpool.io:8888
 set WALLET=0x69593a8875894bed5665a71d8bd0fdd9980cac93
 set WORKERNAME=WorkerName
 set CONFIG=-U --farm-recheck 200
